e7f18e49a4cb40df7374369c479521b09884841a
hp <fadysamuel18@gmail.com>
2024-02-02 12:49:08 +0200

    initial islamy project empty home screen

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  static const routeName = 'HomeScreen' ;
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+
+    );
+  }
+}
